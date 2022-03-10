089265be52dd608fd043cfaca714990b82127895
rayaalmudena <raya.almudena@gmail.com>
2022-03-10 12:42:46 +0100

    ya esta

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
+buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+        body.style.backgroundColor=event.target.id;
+    });
+});
+
